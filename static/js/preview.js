c6d9c20534cdf6e953587fb5130937fc86f257c5
sch7878 <sch8787@naver.com>
2022-12-13 11:48:42 +0900

    [update]카카오톡 공유하기 버튼 기능 생성

--- static/js/preview.js
+++ static/js/preview.js
@@ -1,3 +1,9 @@
 function call(phoneNumber) {
   location.href = "tel:" + phoneNumber;
 }
+
+function shareMessage() {
+    Kakao.Share.sendScrap({
+      requestUrl: 'http://localhost:5000/',
+    });
+  }
