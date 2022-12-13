9b7fe8d570a15d4021c9d5849a75834f41519373
sch7878 <sch8787@naver.com>
2022-12-13 15:24:17 +0900

    [update]지도 기능 추가

--- static/js/preview.js
+++ static/js/preview.js
@@ -88,12 +88,17 @@ var map = new kakao.maps.Map(mapContainer, mapOption);
 var geocoder = new kakao.maps.services.Geocoder();
 
 // 주소로 좌표를 검색합니다
-geocoder.addressSearch('강남구 강남대로 652', function(result, status) {
+geocoder.addressSearch(document.getElementById('wedding-hall-address').innerText, function(result, status) {
 
     // 정상적으로 검색이 완료됐으면
      if (status === kakao.maps.services.Status.OK) {
 
         var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        mapOption = {
+        center: kakao.maps.LatLng(result[0].y, result[0].x), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
 
         // 결과값으로 받은 위치를 마커로 표시합니다
         var marker = new kakao.maps.Marker({
@@ -103,7 +108,7 @@ geocoder.addressSearch('강남구 강남대로 652', function(result, status) {
 
         // 인포윈도우로 장소에 대한 설명을 표시합니다
         var infowindow = new kakao.maps.InfoWindow({
-            content: '<div style="width:150px;text-align:center;padding:6px 0;">식장</div>'
+            content: document.getElementById('wedding-hall-name').innerText
         });
         infowindow.open(map, marker);
 
